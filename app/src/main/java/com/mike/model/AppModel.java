b952e9ca22e1a490b8edde051ad75ec891ceaca2
mike20132013 <michael.tirkey2012@yahoo.com>
2014-05-10 10:48:58 -0400

    Test 3

--- app/src/main/java/com/mike/model/AppModel.java
+++ app/src/main/java/com/mike/model/AppModel.java
@@ -10,6 +10,9 @@ public class AppModel {
     Context context;
     private Double latitude;
     private Double longitude;
+    String currentCity;
+    String currentCountry;
+    String currentZip;
 
     public AppModel(Context context){
 
